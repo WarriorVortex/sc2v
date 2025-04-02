3fcceb02354f9bcaac0656bcacc732aa667b4091
Vladimir Erokhin <erohinvova01010111@gmail.com>
2025-04-02 16:09:48 +0300

    Fixed .bat and .sh scripts to build the project

--- bin/sc2v.sh
+++ bin/sc2v.sh
@@ -1,11 +1,34 @@
-#!/bin/sh
-#
-# script to perform 2 steps of sc2v for input module 
-# and remove temporary files
-#
-# usage: sc2v <filename_without_extension>
-#
-../bin/sc2v_step1 < $1.cpp
-../bin/sc2v_step2 < $1.h > $1.sc2v
-../bin/sc2v_step3 < $1.sc2v > $1.v
-\rm *.sc2v
+#!/bin/bash
+
+if [ -z "$1" ]; then
+    echo "SystemC 2 Verilog converter"
+    echo "Usage: $0 <Module name without extension>"
+    exit 1
+fi
+
+MODULE="$1"
+SCRIPT_DIR=$(dirname "$0")
+
+check_file() {
+    if [ ! -f "$1" ]; then
+        echo "Can't find $1"
+        exit 1
+    fi
+}
+
+check_file "${MODULE}.cpp"
+check_file "${MODULE}.h"
+
+echo "Processing ${MODULE}"
+"${SCRIPT_DIR}/sc2v_step1" < "${MODULE}.cpp" || exit 1
+
+echo "Processing ${MODULE}.h"
+"${SCRIPT_DIR}/sc2v_step2" < "${MODULE}.h" > "${MODULE}.sc2v" || exit 1
+
+echo "Generating verilog file"
+"${SCRIPT_DIR}/sc2v_step3" < "${MODULE}.sc2v" > "${MODULE}.v" || exit 1
+
+echo "Done"
+
+# Cleanup
+rm -f "${MODULE}.sc2v" *.sc2v 2>/dev/null
